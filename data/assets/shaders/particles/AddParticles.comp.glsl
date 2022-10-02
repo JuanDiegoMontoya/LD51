80370dd2ada3e2e96a7e1cc1f7fad72ef086be96
Jake <>
2022-10-02 01:39:15 -0700

    particle prep stuff

---
+++ data/assets/shaders/particles/AddParticles.comp.glsl
@@ -0,0 +1,44 @@
+#version 460 core
+
+struct Particle
+{
+  vec2 position;
+  uvec2 emissive; // packed 16-bit float RGBA
+  uint velocity; // packed 16-bit float XY
+  uint flags;
+};
+
+layout(std430, binding = 0) writeonly restrict buffer ParticlesBuffer
+{
+  Particle list[];
+}outParticles;
+
+layout(std430, binding = 1) coherent restrict buffer TombstonesBuffer
+{
+  coherent int size;
+  int indices[];
+}tombstones;
+
+layout(std430, binding = 2) readonly restrict buffer ParticlesCopyBuffer
+{
+  Particle list[];
+}inParticles;
+
+layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+void main()
+{
+  uint index = gl_GlobalInvocationID.x;
+  if (index >= inParticles.list.length())
+    return;
+
+  // undo decrement and return if nothing in freelist
+  int indexIndex = atomicAdd(tombstones.size, -1) - 1;
+  if (indexIndex < 0)
+  {
+    atomicAdd(tombstones.size, 1);
+    return;
+  }
+
+  int particleIndex = tombstones.indices[indexIndex];
+  outParticles.list[index] = inParticles.list[particleIndex];
+}
---
+++ data/assets/shaders/particles/UpdateParticles.comp.glsl
@@ -0,0 +1,109 @@
+#version 460 core
+
+struct Particle
+{
+  vec2 position;
+  uvec2 emissive; // packed 16-bit float RGBA
+  uint velocity; // packed 16-bit float XY
+  uint flags;
+};
+
+layout(std430, binding = 0) restrict buffer ParticlesBuffer
+{
+  Particle list[];
+}particles;
+
+layout(std430, binding = 1) coherent restrict buffer TombstonesBuffer
+{
+  coherent int size;
+  int indices[];
+}tombstones;
+
+layout(std430, binding = 2) coherent restrict buffer RenderIndicesBuffer
+{
+  coherent int size;
+  int indices[];
+}renderIndices;
+
+layout(std140, binding = 0) uniform Uniforms
+{
+  float dt;
+  float magnetism;
+  vec2 cursorPosition;
+}uniforms;
+
+shared int sh_freeIndex;
+shared int sh_requestedFreeIndices;
+shared int sh_drawIndex;
+shared int sh_requestedDrawIndices;
+
+layout(local_size_x = 128, local_size_y = 1, local_size_z = 1) in;
+void main()
+{
+  // strategy: record how many elements we actually need, request it at once, then write to buffers
+
+  if (gl_LocalInvocationIndex == 0)
+  {
+    sh_requestedFreeIndices = 0;
+    sh_requestedDrawIndices = 0;
+  }
+
+  barrier();
+  memoryBarrierShared();
+
+  int index = int(gl_GlobalInvocationID.x);
+  bool needFreeIndex = false;
+  bool needDrawIndex = false;
+  if (index < particles.list.length())
+  {
+    Particle particle = particles.list[index];
+    vec2 velocity = unpackHalf2x16(particle.velocity);
+    vec2 acceleration = vec2(1.0) / min(vec2(0.1), (uniforms.magnetism * (uniforms.cursorPosition - particle.position)));
+
+    float life = 1.0;
+    if (life > 0.0)
+    {
+      velocity += acceleration * uniforms.dt;
+      particle.position += velocity * uniforms.dt;
+      particle.velocity = packHalf2x16(velocity);
+
+      // if (life <= 0.0) // particle just died
+      // {
+      //   needFreeIndex = true;
+      //   atomicAdd(sh_requestedFreeIndices, 1);
+      // }
+      // else
+      // {
+         // particle is alive, so we will render it (add its index to drawIndices)
+         needDrawIndex = true;
+         atomicAdd(sh_requestedDrawIndices, 1);
+      // }
+    }
+
+    particles.list[index] = particle;
+  }
+
+  barrier();
+  memoryBarrierShared();
+
+  if (gl_LocalInvocationIndex == 0)
+  {
+    sh_freeIndex = atomicAdd(tombstones.size, sh_requestedFreeIndices);
+    sh_drawIndex = atomicAdd(renderIndices.size, sh_requestedDrawIndices);
+  }
+
+  barrier();
+  memoryBarrierShared();
+
+  if (needFreeIndex)
+  {
+    int freeIndex = atomicAdd(sh_freeIndex, 1);
+    tombstones.indices[freeIndex] = index;
+  }
+
+  if (needDrawIndex)
+  {
+    uint drawIndex = atomicAdd(sh_drawIndex, 1);
+    renderIndices.indices[drawIndex] = index;
+  }
+}
